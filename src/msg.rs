56a4ac9ee68f3b5c0e0d1b467e80d889ac3c0775
Will Gardiner <gardiner.w@gmail.com>
2020-10-16 16:02:34 -0400

    initial commit

---
+++ src/msg.rs
@@ -0,0 +1,41 @@
+use cosmwasm_std::{Coin, HumanAddr};
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct InitMsg {
+    // pub count: i32,
+    // Coins for funding pool are attached to TX
+    pub proposer_whitelist: Vec<HumanAddr>,
+    // pub voter_whitelist: Vec<HumanAddr>,
+    // pub proposal_min_period: Option<u32>,
+    // pub voting_min_period: Option<u32>,
+    // pub proposal_period_start: Option<u64>,
+    // pub proposal_period_end: Option<u64>,
+    // pub voting_period_start: Option<u64>,
+    // pub voting_period_end: Option<u64>,
+    // pub funding_formula: Option<String>,
+}
+
+
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum HandleMsg {
+    Increment {},
+    Reset { count: i32 },
+
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    // GetCount returns the current count as a json-encoded number
+    GetCount {},
+}
+
+// We define a custom struct for each query response
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct CountResponse {
+    pub count: i32,
+}
---
+++ src/state.rs
@@ -0,0 +1,53 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+use cosmwasm_std::{CanonicalAddr, Storage, Coin};
+use cosmwasm_storage::{singleton, singleton_read, ReadonlySingleton, Singleton};
+
+pub static CONFIG_KEY: &[u8] = b"config";
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct State {
+    pub count: i32,
+    pub owner: CanonicalAddr,
+    // funding pool is stored in contract balance?
+    // pub balance: Vec<Coin>,
+    pub proposer_whitelist: Vec<CanonicalAddr>,
+    // pub voter_whitelist: Vec<CanonicalAddr>,
+    // // pub voting_min_period: u32, // in seconds
+    // // pub proposal_min_period: u32,
+    // // pub min_voting_period: u32,
+    // // pub min_proposal_period: u32,
+    // pub proposal_period_start: Option<u64>, // Option values are optional
+    // pub proposal_period_end: Option<u64>,
+    // pub voting_period_start: Option<u64>,
+    // pub voting_period_end: Option<u64>,
+    // pub funding_formula: Option<String>,
+    pub proposals: Vec<Proposal>,
+    pub votes: Vec<Vote>,
+}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+pub struct Proposal {
+    pub id: u32,
+    pub name: String,
+    pub recipient: CanonicalAddr,
+    pub description: String,
+    pub tags: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+pub struct Vote {
+    pub voter: CanonicalAddr,
+    pub proposal: u32, // reference to proposal id
+    // pub txid, // would this be valuable?
+    pub amount: u32, // can this just be referenced from the contract's trasaction history?
+}
+
+pub fn config<S: Storage>(storage: &mut S) -> Singleton<S, State> {
+    singleton(storage, CONFIG_KEY)
+}
+
+pub fn config_read<S: Storage>(storage: &S) -> ReadonlySingleton<S, State> {
+    singleton_read(storage, CONFIG_KEY)
+}
