52f19a88b8d789e016d994896bfade488cd70b5d
paul121 <paul.weidner@gmail.com>
2020-10-23 11:37:58 -0700

    Update project name to build schema.

--- examples/schema.rs
+++ examples/schema.rs
@@ -3,8 +3,8 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use project_name::msg::{HandleMsg, InitMsg, QueryMsg, StateResponse};
-use project_name::state::State;
+use funding_contract::msg::{HandleMsg, InitMsg, QueryMsg, StateResponse};
+use funding_contract::state::State;
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
